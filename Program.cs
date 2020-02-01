ef4f70ee0cfd53caefe6c701366f6edcd4e82d2f
cnrdmrci <canerdemirci.mail@gmail.com>
2020-02-01 16:11:35 +0300

    project created

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Usage_Of_MongoDb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.ReadLine();
+        }
+    }
+}
